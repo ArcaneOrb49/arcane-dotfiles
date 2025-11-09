41ad685d6d4102de91fa8a9beef924af9d204d22
Arvid <arvid.meltzer@gmail.com>
2025-11-09 20:56:32 +0100

    cache/wal/ removed

--- cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#eee3d0", "#0d0f12" },
-	[SchemeSel] = { "#eee3d0", "#425A92" },
-	[SchemeOut] = { "#eee3d0", "#DBA7A1" },
-};
--- cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#0d0f12";
-static const char* selfgcolor   = "#eee3d0";
-static const char* normbgcolor  = "#666D91";
-static const char* normfgcolor  = "#eee3d0";
-static const char* urgbgcolor   = "#425A92";
-static const char* urgfgcolor   = "#eee3d0";
