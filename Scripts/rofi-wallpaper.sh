c601e17bf2119f137b69c405f9f55f8abb4d87c5
Arvid <arvid.meltzer@gmail.com>
2025-12-28 13:48:56 +0100

    Add portal-desert theme

--- Scripts/rofi-wallpaper.sh
+++ Scripts/rofi-wallpaper.sh
@@ -1,23 +1,4 @@
 #!/usr/bin/env bash
-#set -euo pipefail
-# Directory with wallpapers
-#DIR="$HOME/arcanes-dotfiles/wallpapers"
-
-# Let the user choose
-#sel="$(find "$DIR" -type f | rofi -dmenu -i -p 'Wallpaper')"
-
-# If the user picked something, tell hyprpaper to use it
-#if [ -n "$sel" ]; then
-#    hyprctl hyprpaper unload all
-#    hyprctl hyprpaper preload "$sel"
-#    hyprctl hyprpaper wallpaper ",$sel"
-#    wal -n -q -i "$sel"
-#    pkill waybar
-#    waybar & disown
-    
-
-#fi
-#!/usr/bin/env bash
 DIR="$HOME/arcanes-dotfiles/wallpapers"
 
 # Make an associative array: key=filename, value=fullpath
